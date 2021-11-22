875dd02561df3a91520650f531390d96bbabf3e2
estebanmavarezb <emavarez@glamit.com.ar>
2021-11-22 20:25:56 -0300

    problema del name

--- react/componentes/ShortDescription/index.tsx
+++ react/componentes/ShortDescription/index.tsx
@@ -1,15 +1,14 @@
 import React, { Fragment, useMemo} from 'react';
 import styles from './styles.css';
 import { useProduct } from 'vtex.product-context';
-
+import { pathOr } from 'ramda'
 interface PropsI {
     isShow: boolean,
 }
 const ShortDescription = (props: PropsI) => {
     const {isShow} = props
     const { product } = useProduct();
-    const shortName = product?.items[0]?.complementName
-
+    const shortName = pathOr('', ['items', 0, 'complementName'], product)
     return useMemo(() =>{
         return (
             isShow ?
